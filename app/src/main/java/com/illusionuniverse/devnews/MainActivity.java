3d79481d52dcd6c726ac91a2a028d66fb41a6eca
Amit Kumar <104451914+amitthecoder@users.noreply.github.com>
2022-08-12 15:49:40 +0530

    Initial commit

---
+++ app/src/main/java/com/illusionuniverse/devnews/MainActivity.java
@@ -0,0 +1,49 @@
+package com.illusionuniverse.devnews;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recview;
+    myadapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recview=(RecyclerView)findViewById(R.id.recview);
+        recview.setLayoutManager(new LinearLayoutManager(this));
+
+        FirebaseRecyclerOptions<model> options =
+                new FirebaseRecyclerOptions.Builder<model>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("news"), model.class)
+                        .build();
+
+        adapter=new myadapter(options);
+        recview.setAdapter(adapter);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+}
